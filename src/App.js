b752a149c05d541878932d94fccd795f7ae76856
Israel <israelvillela2016@gmail.com>
2022-05-26 15:03:44 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [advice, setAdvice]=useState('')
+
+    const buttonHandler=()=>{
+        fetch('https://api.adviceslip.com/advice')
+        .then((response) => response.json())
+        .then((data) => setAdvice(data.slip.advice))
+        
+    }
+
+    useEffect(()=>{
+        buttonHandler()
+    },[])
+    
+
+    return ( 
+        <div className='app'>
+            <div className='card'>
+                <h1 className='heading'>{advice}</h1>
+
+                <button className='button' onClick={buttonHandler} >
+                    <span>Give Me Advice</span>
+                </button>
+            </div>
+        </div>
+     );
 }
-
+ 
 export default App;
